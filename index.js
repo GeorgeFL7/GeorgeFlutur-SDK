5bd95b416eae9690c8cef9e2348115b50b9df0b1
George Flutur <77577235+GeorgeFL7@users.noreply.github.com>
2022-10-04 12:49:38 +0300

    first api call

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-n
+import axios from "axios";
+
+let response = await axios.get('https://the-one-api.dev/v2/book')
+    .catch(function (error) {
+        console.log(error)
+    });
+console.log(response.data);

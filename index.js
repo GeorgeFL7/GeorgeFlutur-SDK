9aa315d71e22edc0e568de02d13261ae859d1bdc
George Flutur <77577235+GeorgeFL7@users.noreply.github.com>
2022-10-04 12:54:40 +0300

    Class wrapping

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 import axios from "axios";
 
-let response = await axios.get('https://the-one-api.dev/v2/book')
-    .catch(function (error) {
-        console.log(error)
-    });
-console.log(response.data);
+
+class LotRLibrary {
+    constructor(token = null) {
+        this.token = token;
+    }
+    async getBooks() {
+        let response = await axios.get('https://the-one-api.dev/v2/book')
+            .catch(function (error) {
+                console.log(error)
+            });
+        return response?.data;
+    }
+}
+
+const lotrlib = new LotRLibrary()
+console.log(await lotrlib.getBooks());
+

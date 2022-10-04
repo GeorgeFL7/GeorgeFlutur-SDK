115f1470b4e24da229a5eabc3c62e8bbc7d2be3e
George Flutur <77577235+GeorgeFL7@users.noreply.github.com>
2022-10-04 14:46:35 +0300

    Added auth api call; added tests; added dotenv

--- test/test.js
+++ test/test.js
@@ -1,10 +1,21 @@
 import assert from "assert";
 import { LotRLibrary, BASE_URL } from "../lib/LotRLibrary.js"
-
-
-describe('The LotR SDK shold ', function () {
+import dotenv from "dotenv"
+dotenv.config()
+describe('The LotR SDK should ', function () {
+    const lotrlib = new LotRLibrary(process.env.LOTR_API_KEY)
     it('have a defined URL', function () {
         assert.notEqual('undefined', BASE_URL);
     })
+
+    it('return 3 books on the /book endpoint', async function () {
+        const booksObject = await lotrlib.getBooks();
+        assert.equal(3, booksObject.total);
+    })
+    it('return 8 movies on the /movie endpoint', async function () {
+        const booksObject = await lotrlib.getMovies();
+        assert.equal(8, booksObject.total);
+    })
+
 
 })
